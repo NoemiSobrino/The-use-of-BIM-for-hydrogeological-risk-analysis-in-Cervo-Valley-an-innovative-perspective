aa056367ed2ee6c3cdcf622140a0e279a5aa0a73
NoemiSobrino <noemi.sobrino@ugent.be>
2024-06-04 20:38:45 +0200

    Update PluviometerAPI.py

--- 2D map/Codes/PluviometerAPI.py
+++ 2D map/Codes/PluviometerAPI.py
@@ -58,37 +58,3 @@ def call_API():
 # Attivo la modalità di debug
 if __name__ == "__main__":
     app.run(debug=True)
-
-    
-# Funzione per creare il grafico -> Commentata perché il grafico lo creiamo con ChartJS
-# In ingresso passo i dati restituiti dall'API di ARPA Piemonte
-# def plot_data(data, location, reqDates):
-#     print(location, reqDates)
-#     # Imposto la dimensione del grafico
-#     plt.figure(figsize=(70, 30))
-#     # Inizializzo due array di date e valori di ptot
-#     dates = []
-#     ptot_values = []
-#     # Esegui un ciclo per ottenere i dati di ptot
-#     for entry in data['results']:
-#         date = datetime.strptime(entry['data'], '%Y-%m-%d') # Salvo la data in 'entry' convertita nel formato 'YYYY-MM-DD'
-#         ptot_value = entry.get('ptot') # Salvo il valore di ptot in 'entry'
-#         # Controllo se ci sono dati di ptot trovati
-#         if ptot_value is not None:
-#             dates.append(date) # Aggiungo in fondo alla lista 'dates' il valore di 'date'
-#             ptot_values.append(ptot_value) # Aggiungo in fondo alla lista 'ptot_values' il valore di 'ptot_value'
-#     plt.bar(dates, ptot_values, color='blue')  # Plotta il grafico con tutte le date sull'asse x
-#     plt.xlabel('Date', fontsize=50, fontweight='bold') # Imposto la labale dell'asse x
-#     plt.ylabel('Total rainfall (mm)', fontsize=50, fontweight='bold') # Imposto la labale dell'asse y
-#     plt.title(f"RAINFALL in {location} on {reqDates['min_date']} to {reqDates['max_date']}", fontsize=50, fontweight='bold') # Imposto il titolo nel grafico
-#     plt.xticks(fontsize=50)  # Modifica la dimensione del testo sull'asse x
-#     plt.yticks(fontsize=50)  # Modifica la dimensione del testo sull'asse y
-#     plt.gca().xaxis.set_major_locator(DayLocator(interval=1)) # Imposta lo spaziamento tra le date sull'asse x
-#     plt.grid(True) # Mostro la griglia
-#     #plt.tight_layout() # Imposto il layout del grafico
-#     #plt.show()
-#     my_stringIObytes = io.BytesIO() # Creo un oggetto BytesIO che mi permette di lavorare in un sistema binario dentro la memoria del PC
-#     plt.savefig(my_stringIObytes, format='jpg') # Salvo l'immagine nel file BytesIO come png
-#     my_stringIObytes.seek(0) # Permette la lettura dei dati dalla prima posizione del base64
-#     my_base64_jpgData = {'data': data, 'image_url': base64.b64encode(my_stringIObytes.read()).decode()} # Creo un dizionario Python con i dati dell'immagine e quello che mi restituisce l'API
-#     return my_base64_jpgData # Restituisco il dizionario per JS
